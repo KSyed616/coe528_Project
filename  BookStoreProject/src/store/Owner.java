bc940b299d4d8ac477a9577fbb574ded8d2f60ce
Unnati <Unnati@192.168.2.15>
2021-04-02 18:31:03 -0400

    Merge origin/master
    Conflicts:
    	 BookStoreProject/src/store/LogIn.java

---  BookStoreProject/src/store/Owner.java
+++  BookStoreProject/src/store/Owner.java
@@ -13,16 +13,19 @@ public class Owner extends User{
     private String password;
     
     private String data = "Name Of The Wind,50\n"
-            + "The Way Of Kings,45";    
+            + "The Way Of Kings,45\n"
+            + "The Wise Man's Fear,60";    
     
     private String data2 = "Unnati,pass,Gold,10000";
     
+    //Used for "Book.txt".
     File file;
     FileReader out; 
     FileWriter in; 
     BufferedWriter write;
     BufferedReader read; 
     
+    //Used for "Customer.txt". 
     File file2;
     FileReader out2; 
     FileWriter in2; 
@@ -68,10 +71,11 @@ public class Owner extends User{
     }
     
     //method to remove books. 
-    public void removeBooks(String name, double price) throws IOException{        
+    public void removeBooks(String name, double price) throws IOException{         
+        //FileReader reads directly from the file. BufferedReader reads from the FileReader. FileReader is slower. 
         out = new FileReader("Book.txt");
         read = new BufferedReader(out);
-        
+ 
         String thisLine;
         String data = "";
         String newLine;
@@ -82,8 +86,7 @@ public class Owner extends User{
         newLine = data.replaceAll(name, "DELETED");
         in = new FileWriter("Book.txt");
         in.write(newLine);
-        in.close();
-        
+        in.close();   
     }
     
     public void initializeBookStore() throws IOException{
@@ -97,8 +100,7 @@ public class Owner extends User{
 
         try{
             out = new FileReader("Book.txt");
-            BufferedReader read  = new BufferedReader(out);
-
+            BufferedReader read  = new BufferedReader(out); 
             String line;
 
             while((line  = read.readLine()) != null){
@@ -113,14 +115,21 @@ public class Owner extends User{
         } catch (FileNotFoundException ex) {         
         } catch (IOException ex) {                    
         }
+        
         if(file.length() == 0 || num_of_lines == num_of_del){
+            //if file is empty, writes data string into file. 
             write.write(data);
             write.newLine();
             write.close();
         }
     }
+    
+    //method to initializeCust. When file is empty, writes the initialied customer info into it. 
     public void initializeCust() throws IOException{
+        //Creates "Customer.txt" file. 
         file = new File("Customer.txt");
+        
+        //Adds to the "Customer.txt" file. 
         in = new FileWriter("Customer.txt", true);
         write = new BufferedWriter(in);
        
@@ -149,40 +158,48 @@ public class Owner extends User{
         }
         System.out.println(num_of_lines+" "+num_of_del);
         if(file.length() == 0 || num_of_lines-2 == num_of_del){
+        //If file is empty, writes data2 string into the file. 
+        if(file.length() == 0){
+ 
             write.write(data2);
             write.newLine();
             write.close();
         }
+        }
     }
-    //method to add books. 
-    public void addBooks(String name, double price) throws IOException{       
+    
+    //method to add books. Adds onto the already created "Book.txt" file. 
+    public void addBooks(String name, double price) throws IOException{   
+        //Adds onto the already created "Book.txt" file. 
         in = new FileWriter("Book.txt", true);
         out = new FileReader("Book.txt");
         write = new BufferedWriter(in);
         
+        //creates object for Book. 
         Book b = new Book(name, price);   
+        //adds object "Book" and its parameters "name" and "price" into the file. 
         write.write(""+b.getName()+","+b.getPrice());  
         write.newLine();
-        
         write.close();
     }
     
     //method to add customers. 
-    public void addCust(String userName, String password, String status, double Point) throws IOException{  
+    public void addCust(String userName, String password, String status, double Point) throws IOException{
+        //Adds on to the "Customers.txt" file. 
         in2 = new FileWriter("Customer.txt", true);
         out2 = new FileReader("Customer.txt");
         write2 = new BufferedWriter(in2);
         
+        //Creates object for Customer. 
         Customer c = new Customer(userName, password, status, Point); 
+        //adds object "Customer" and its parameters "userName", "password", "status", "Point" into the file. 
         write2.write(""+c.getUserName()+","+c.getPassword()+","+c.getStatus()+","+c.getPoint());  
-        write2.newLine();
-        
+        write2.newLine(); 
         write2.close();
     }
     
-    //method to remove customers. 
+    //method to remove customers. Removes customers from the already created "Customer.txt" file. 
     public void removeCust(String userName, String password, String status, double Point) throws IOException{
-
         out2 = new FileReader("Customer.txt");
         read2 = new BufferedReader(out2);
         
@@ -193,6 +210,8 @@ public class Owner extends User{
             data += thisLine + "\r\n";
         }
         read2.close();
+        
+        //Line of data is replced with "deleted". 
         newLine = data.replaceAll(userName, "DELETED");
         in2 = new FileWriter("Customer.txt");
         in2.write(newLine);
@@ -211,12 +230,12 @@ public class Owner extends User{
     }
 
     @Override
-    public void state_change(Stage primaryStage) {
+    public void state_change(Stage primaryStage){
         LogInFX l = new LogInFX();
         l.start(primaryStage);
     }
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         Owner o = new Owner("ads", "ad");
         o.initializeCust();
     }
