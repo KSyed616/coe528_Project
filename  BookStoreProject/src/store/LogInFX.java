22f3bd6f1d1374cf9880256ed7b9a847b220f33e
syedk <syedk@192.168.10.3>
2021-04-03 17:01:58 -0400

    Merge origin/master
    Conflicts:
    	 BookStoreProject/src/store/LogIn.java

---  BookStoreProject/src/store/LogInFX.java
+++  BookStoreProject/src/store/LogInFX.java
@@ -6,6 +6,8 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -14,9 +16,11 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class LogInFX extends Application {
@@ -56,7 +60,7 @@ public class LogInFX extends Application {
                 
                 root.getChildren().clear();
                 
-                if(userName.equalsIgnoreCase("admin")){
+                if(userName.equalsIgnoreCase("admin") && password.equalsIgnoreCase("admin")){
                     OwnerFX o = new OwnerFX(userName, password);
                     o.start(primaryStage);
                 }
@@ -65,6 +69,9 @@ public class LogInFX extends Application {
                     if(cust.checkCust(userName, password) == true){
                         CustomerFX c = new CustomerFX(userName, password);
                         c.makeCust(primaryStage, userName, password);
+                    }
+                    else{
+                        error(primaryStage);
                     }
                 }
             }
@@ -83,8 +90,26 @@ public class LogInFX extends Application {
         primaryStage.setTitle("Login");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
     }
-
+    private void error(Stage primaryStage) {
+        
+        Button close = new Button("Close");
+        VBox vbox = new VBox(new Text("The information you entered is not a part of our database"), close);
+        
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(15));
+        close.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                primaryStage.close();
+            }
+        });
+        
+        primaryStage.setScene(new Scene(vbox));
+        primaryStage.show();
+        
+    }
     public static void main(String[] args) {
         launch(args);
     }
